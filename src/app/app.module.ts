e0a6d9b4b95d68fc123618fc8d41f3887f5958da
tuncayaltunn <taltun0855@gmail.com>
2025-01-08 22:59:23 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from "./admin/layout/layout.module";
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    AdminModule,
+    UiModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
