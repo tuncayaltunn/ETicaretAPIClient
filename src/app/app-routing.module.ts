7a520633af09371162a7d2ff05e81ceca23d9468
tuncayaltunn <taltun0855@gmail.com>
2025-01-09 22:26:16 +0300

    Multiple Layout Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "admin", component: LayoutComponent, children: [
+      { path:"customer", loadChildren : () => import("./admin/components/customer/customer.module")
+        .then(module => module.CustomerModule)},
+      { path:"products", loadChildren : () => import("./admin/components/products/products.module")
+          .then(module => module.ProductsModule },
+      { path:"customer", loadChildren : () => import("./admin/components/order/order.module")
+            .then(module => module.OrderModule) }
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
