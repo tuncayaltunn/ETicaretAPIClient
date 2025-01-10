31777ff14aa6a7de555d721faa490743907a3c6f
tuncayaltunn <taltun0855@gmail.com>
2025-01-10 16:52:48 +0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from "./admin/layout/layout.module";
+
 import { AdminModule } from './admin/admin.module';
 import { UiModule } from './ui/ui.module';
 
