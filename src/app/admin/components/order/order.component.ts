ea8cefbdf4e88fe5fd9fd4f30a3e753f3b467b34
tuncayaltunn <taltun0855@gmail.com>
2025-01-12 23:51:59 +0300

    Spinners in BaseComponent

--- src/app/admin/components/order/order.component.ts
+++ src/app/admin/components/order/order.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent extends BaseComponent implements OnInit {
 
-  constructor(private spinner: NgxSpinnerService) { }
+  constructor(spinner: NgxSpinnerService) {
+    super(spinner)
+   }
 
   ngOnInit(): void {
-    this.spinner.show()
-
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 5000);
+    this.showSpinner(SpinnerType.BallSpinClockwiseFadeRotating)
   }
 
 }
---
+++ src/app/base/base.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService, Spinner } from 'ngx-spinner';
+
+
+export class BaseComponent {
+
+  /**
+   *
+   */
+  constructor(private spinner : NgxSpinnerService) {}
+
+  showSpinner(spinnerNameType : SpinnerType){
+    this.spinner.show(spinnerNameType);
+
+    setTimeout(() => this.spinner.hide(spinnerNameType),1000);
+  }
+
+  hideSpinner(spinnerNameType : SpinnerType){
+    this.spinner.hide(spinnerNameType)
+  }
+
+}
+
+export enum SpinnerType{
+  BallScaleMultiple = "s1",
+  BallAtom = "s2",
+  BallSpinClockwiseFadeRotating = "s3"
+}
